436ee4ad0fff9e06722b153d3bd456d1f7f9961c
jp-knj <kenji.tomita1996@gmail.com>
2022-07-13 20:44:02 +0900

    :+1: process user input with inquirer in node

--- index.mjs
+++ index.mjs
@@ -1,11 +1,33 @@
-import readline from "node:readline/promises";
+import inquirer from "inquirer";
 
-const rl = readline.createInterface({
-    terminal: true,
-    input: process.stdin,
-    output: process.stdout,
+const addQuestion = async () =>{
+    const answers = await inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of the item?"
+        },
+        {
+            type: "list",
+            name: "live",
+            message: "Where do you live?",
+            choices: ["Japan", "Korean", "Other"]
+        },
+    ]);
+    console.log("Your name is " + answers.name)
+    console.log("You live in " + answers.live)
+}
+
+const flags = [];
+
+process.argv.forEach(arg => {
+    if (/^-/.test(arg)) {
+        flags.push(arg.replaceAll("-", ""));
+    }
 });
 
-const answer = await rl.question("What is your name? ");
-console.log("your name is: " + answer);
-rl.close();
+if (flags.includes("a") || flags.includes("add")) {
+    addQuestion();
+} else {
+    addQuestion();
+}
