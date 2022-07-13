e8a3bdd33e661567bda9b97574938c8393840494
jp-knj <kenji.tomita1996@gmail.com>
2022-07-13 20:05:41 +0900

    :sparkles: Parse user input through nodejs process argv for flags

---
+++ index.js
@@ -0,0 +1,15 @@
+const flags = [];
+
+process.argv.forEach((arg) => {
+    if (/^-/.test(arg)) {
+        flags.push(arg.replaceAll("-", ""));
+    }
+});
+
+console.log(flags);
+
+if(flags.includes("a") || flags.includes("add")) {
+    console.log("add some values");
+} else {
+    console.log("no values to add");
+}
